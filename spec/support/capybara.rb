02e568f7edbf0ce8e0fbe07cd7cb9fe178b2c190
Marsel Mustafin <marsel.mustafin@flatstack.com>
2018-10-03 12:25:01 +0300

    One more attempt

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,5 +3,6 @@ Capybara.configure do |config|
   config.always_include_port = true
   config.javascript_driver = :webkit
   config.asset_host = "http://#{ENV.fetch('HOST')}"
-  config.app_host = "http://lvh.me:5000"
+  config.app_host = "http://#{ENV.fetch('HOST')}"
+  config.default_max_wait_time = 10
 end
