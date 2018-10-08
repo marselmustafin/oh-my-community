fed5926f7a33d9b33510872c364c5785bdd78bf9
Marsel Mustafin <Mustafinkzn97@gmail.com>
2018-10-08 10:34:17 +0300

    Change capybara driver (#5)
    * Add post related specs
    
    * Fix js typo and domain generating
    
    * Attempt to fix failed ci tests
    
    * One more attempt
    
    * Refactor specs
    
    * Update gems
    
    * Replace Capybara webkit by Selenium

---
+++ spec/features/posts/create_rating_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+feature "Create Rating" do
+  include_context "current user signed in"
+
+  let!(:post) { create :post, community: current_community }
+
+  background do
+    visit_community current_community, post_path(post)
+
+    find("img[alt='4']").click
+  end
+
+  scenario "User rate some post" do
+    expect(page).to have_content("Your rating successfully submitted!")
+  end
+
+  context "User rated this post that he already" do
+    background { create :rating, user: current_user, post: post, value: 3 }
+
+    scenario "User rate some post" do
+      expect(page).not_to have_content("Your rating successfully submitted!")
+    end
+  end
+end
