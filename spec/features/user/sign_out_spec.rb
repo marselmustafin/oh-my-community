a8178b8c10ba49095872274793350bfc3472eaac
Marsel Mustafin <marsel.mustafin@flatstack.com>
2018-10-15 11:06:18 +0300

    Update specs

--- spec/features/user/sign_out_spec.rb
+++ spec/features/user/sign_out_spec.rb
@@ -3,10 +3,14 @@ require "rails_helper"
 feature "Sign Out" do
   include_context "current user signed in"
 
+  def click_hidden_header_toggler
+    page.find("#hidden-header-toggler").click
+  end
+
   scenario "User signs out" do
     visit_community current_community
 
-    click_button current_user.full_name
+    click_hidden_header_toggler
     click_link "Sign out"
 
     expect(page).to have_content("Signed out")
